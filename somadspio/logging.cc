175ed2e6ddbf76dc0d259a7d6b13e76bb28345e3
Eric Jonas <jonas@mit.edu>
2009-07-13 11:18:02 -0400

    Forgot to add logging files

---
+++ somadspio/logging.cc
@@ -0,0 +1,29 @@
+#include "somadspio/logging.h"
+#include <boost/logging/format.hpp>
+#include <boost/logging/format/formatter/tags.hpp>
+#include <boost/logging/format/named_write.hpp>
+#include <boost/logging/format/formatter/tags.hpp>
+#include <boost/logging/format/formatter/high_precision_time.hpp>
+
+  //using namespace boost::logging;
+namespace bl = boost::logging; 
+
+
+
+BOOST_DEFINE_LOG_FILTER(somadspio_log_level, somadspio_finder::filter ) // holds the application log level
+BOOST_DEFINE_LOG(somadspio_l, somadspio_finder::logger); 
+
+namespace somadspio {
+    
+  void init_logs()  {
+    
+    somadspio_l()->writer().add_formatter(bl::formatter::high_precision_time("$mm.$ss:$micro ")); 
+    somadspio_l()->writer().add_formatter(bl::formatter::append_newline()); 
+    somadspio_l()->writer().add_destination( bl::destination::cout() );
+    somadspio_l()->mark_as_initialized();
+
+  }
+
+}
+
+
---
+++ somadspio/logging.h
@@ -0,0 +1,33 @@
+#ifndef SOMADSPIO_LOGGING_H
+#define SOMADSPIO_LOGGING_H
+
+#include <boost/logging/format_fwd.hpp>
+#include <boost/logging/format/named_write.hpp>
+
+using namespace boost::logging::scenario::usage;
+typedef use<
+        //  the filter is always accurate (but slow)
+        filter_::change::always_accurate, 
+        //  filter does not use levels
+        filter_::level::use_levels, 
+        // the logger is initialized once, when only one thread is running
+        logger_::change::set_once_when_one_thread, 
+        // the logger favors speed (on a dedicated thread)
+        logger_::favor::speed> somadspio_finder;
+
+
+
+
+BOOST_DECLARE_LOG_FILTER(somadspio_log_level, somadspio_finder::filter ) // holds the application log level
+BOOST_DECLARE_LOG(somadspio_l, somadspio_finder::logger)
+
+namespace somadspio
+{
+#define DSPIOL_(lvl) BOOST_LOG_USE_LOG_IF_LEVEL(somadspio_l(), somadspio_log_level(), lvl )
+
+  void init_logs(); 
+
+}
+
+
+#endif // SOMADSPIO_LOGGING_H
